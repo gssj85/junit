198678c4913733d77a1a19a247a6f9db0bc970ce
Guilherme Souza <guilherme.souza@datum.inf.br>
2023-01-12 18:17:42 -0300

    First commit.

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,53 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BonusServiceTest {
+    @Test
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(
+                new Funcionario(
+                        "Guilherme",
+                        LocalDate.now(),
+                        new BigDecimal("25000")
+                )
+        );
+
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoDoSalario() {
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(
+                new Funcionario(
+                        "Guilherme",
+                        LocalDate.now(),
+                        new BigDecimal("2500")
+                )
+        );
+
+        assertEquals(new BigDecimal("250.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSer10PorCentoParaSalarioDeExatamente10000() {
+        BonusService bonusService = new BonusService();
+        BigDecimal bonus = bonusService.calcularBonus(
+                new Funcionario(
+                        "Guilherme",
+                        LocalDate.now(),
+                        new BigDecimal("10000")
+                )
+        );
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+}
