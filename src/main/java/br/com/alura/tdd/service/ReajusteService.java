b1c5ffc1cf3b18c3ea42efd4fd013d633ee11441
Guilherme Souza <guilherme.souza@datum.inf.br>
2023-01-13 08:17:52 -0300

    Refactoring to Strategy design pattern.

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -7,19 +7,8 @@ import java.math.BigDecimal;
 
 public class ReajusteService {
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho == Desempenho.A_DESEJAR) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+            BigDecimal percentual = desempenho.percentualReajuste();
+            BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
             funcionario.reajustarSalario(reajuste);
-        }
-
-        if (desempenho == Desempenho.BOM) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.reajustarSalario(reajuste);
-        }
-
-        if (desempenho == Desempenho.OTIMO) {
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.reajustarSalario(reajuste);
-        }
     }
 }
